4de6ee1bd2dce5a87ce61ccec28ac2ff2f715695
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-05-17 15:10:28 +0700

    adds hooks variant

---
+++ Hooks/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { authSelectors } from "../redux/auth";
+
+/**
+ * - Если маршрут приватный и пользователь залогинен, рендерит компонент
+ * - В противном случае рендерит Redirect на /login
+ */
+export default function PrivateRoute({
+  isAuthenticated,
+  redirectTo,
+  children,
+  ...routeProps
+}) {
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+
+  return (
+    <Route {...routeProps}>
+      {isLoggedIn ? children : <Redirect to={redirectTo} />}
+    </Route>
+  );
+}
---
+++ Hooks/PublicRoute.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { authSelectors } from "../redux/auth";
+
+/**
+ * - Если маршрут ограниченный, и пользователь залогинен, рендерит редирект на /todos
+ * - В противном случае рендерит компонент
+ */
+export default function PublicRoute({
+  isAuthenticated,
+  redirectTo,
+  children,
+  ...routeProps
+}) {
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+
+  return (
+    <Route {...routeProps}>
+      {isLoggedIn && routeProps.restricted ? (
+        <Redirect to={redirectTo} />
+      ) : (
+        children
+      )}
+    </Route>
+  );
+}
